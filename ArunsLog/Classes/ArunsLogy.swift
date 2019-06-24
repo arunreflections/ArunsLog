e0e9ef455bada3d28240251d238b9a77896a6d63
Arundas MK <arun.das@reflectionsinfos.com>
2019-06-24 14:23:19 +0530

    Add classes for Library

---
+++ ArunsLog/Classes/ArunsLogy.swift
@@ -0,0 +1,16 @@
+//
+//  ArunsLogy.swift
+//  ArunsLog
+//
+//  Created by Arundas MK on 24/06/19.
+//
+
+import Foundation
+import UIKit
+
+open class ArunsLogy: NSObject {
+    
+    open class func MagicLog(msg: String) {
+        print(msg)
+    }
+}
